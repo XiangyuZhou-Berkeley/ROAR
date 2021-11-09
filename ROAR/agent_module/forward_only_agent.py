fab32ee0f224a6c6f124a56cebfb5da585cc6cd2
xiangyu <a375953989@gmail.com>
2021-11-09 12:44:50 -0800

    Merge11.9

--- ROAR/agent_module/forward_only_agent.py
+++ ROAR/agent_module/forward_only_agent.py
@@ -1,7 +1,3 @@
-import time
-
-import numpy as np
-
 from ROAR.agent_module.agent import Agent
 from ROAR.utilities_module.data_structures_models import SensorsData
 from ROAR.utilities_module.vehicle_models import Vehicle, VehicleControl
@@ -16,7 +12,6 @@ class ForwardOnlyAgent(Agent):
     def __init__(self, vehicle: Vehicle, agent_settings: AgentConfig, **kwargs):
         super().__init__(vehicle, agent_settings, **kwargs)
         self.log = deque(maxlen=100)
-        self.start = time.time()
 
     def run_step(self, sensors_data: SensorsData, vehicle: Vehicle) -> VehicleControl:
         super().run_step(sensors_data=sensors_data, vehicle=vehicle)
